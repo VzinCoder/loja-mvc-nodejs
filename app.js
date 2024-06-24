af2366885b512be5f735fdb507dd6e465b4d536b
VzinCoder <contato.viniciusp.b@gmail.com>
2024-06-24 03:55:03 -0300

    add user middleware

--- app.js
+++ app.js
@@ -18,6 +18,32 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.use((req, res, next) => {
+
+    const admin = { name: 'admin', email: 'admin@gmail.com' };
+
+    const createUser = (user) => {
+        let userCreated = null
+        return User.create(user)
+            .then((user) => {
+                userCreated = user
+                return user.createCart()
+            })
+            .then(() => userCreated)
+    }
+
+    const getUser = user => user || createUser(admin);
+
+    const setUserReq = user => {
+        req.user = user;
+        next();
+    };
+
+    User.findByPk(1)
+        .then(getUser)
+        .then(setUserReq)
+        .catch(console.log);
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,13 +55,13 @@ app.use(errorController.get404);
 
 
 User.hasMany(Product)
-Product.belongsTo(User,{constraints:true,onDelete:'CASCADE'})
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' })
 
 User.hasOne(Cart)
 Cart.belongsTo(User)
 
-Cart.belongsToMany(Product,{through:CartItem})
-Product.belongsToMany(Cart,{through:CartItem})
+Cart.belongsToMany(Product, { through: CartItem })
+Product.belongsToMany(Cart, { through: CartItem })
 
 
 
